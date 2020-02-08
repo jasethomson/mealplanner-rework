ddfa7a4eaf850c2106052572ee8e64314d856fa3
jasethomson <jasethomson31@gmail.com>
2020-02-07 16:28:23 -0800

    completed environment setup, added logo and now working on nav

---
+++ client/components/app.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import AppContext from './context';
+import Nav from './nav';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+export default class App extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      placeHolder: false
+    }
+  }
+
+  render(){
+    const contextValue = {
+      placeHolder: this.state.placeHolder
+    };
+    return(
+      <div>
+        <Nav/>
+        {/* <AppContext.Provider value={contextValue}>
+          <Router>
+            <Route exact path="/" component={Home}/>
+          </Router>
+        </AppContext.Provider> */}
+      </div>
+    )
+  }
+}
