31e2e98c55f888d1f8c4974d10714a42e3a9110a
jasethomson <jasethomson31@gmail.com>
2020-02-10 14:21:24 -0800

    added recipe categories page

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import AppContext from './context';
 import Nav from './nav';
 import Home from './home';
+import Recipes from './recipes';
 import Footer from './footer';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
@@ -24,6 +25,7 @@ export default class App extends React.Component{
         <AppContext.Provider value={contextValue}>
           <Router>
             <Route exact path="/" component={Home}/>
+            <Route exact path="/recipes" component={Recipes} />
           </Router>
         </AppContext.Provider>
         <Footer />
